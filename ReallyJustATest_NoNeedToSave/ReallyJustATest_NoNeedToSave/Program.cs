e06e6a12ef21e5f26fac6bdd050bd6010162ee9a
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2021-12-10 20:10:54 +0900

    ReallyJustATest
    testtest

---
+++ ReallyJustATest_NoNeedToSave/ReallyJustATest_NoNeedToSave/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReallyJustATest_NoNeedToSave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class 검사
+    {
+        float HP = 100f;
+        int ATK = 80;
+
+        public void 내려찍기(Monster monster)
+        {
+            monster.HP = monster.HP - ATK;
+        }
+    }
+
+    public class 캐릭터생성
+    {
+        검사 검사1 = new 검사();
+        검사1.내려찍기();
+
+    }
+
+    class Monster
+    {
+        public float HP = 1000f;
+    }
+
+}
